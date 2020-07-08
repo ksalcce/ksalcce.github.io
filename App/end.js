7ae2dfe2cf3b9d30a4449b2fa34351bfa1569f9c
ksalcce <62475617+ksalcce@users.noreply.github.com>
2020-07-07 22:34:53 -0400

    Update end.js

--- App/end.js
+++ App/end.js
@@ -1,3 +1,3 @@
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-finalScore.innerText = mostRecentScore + "/5";
+finalScore.innerText = mostRecentScore + "/5 Questions Correct !!";
